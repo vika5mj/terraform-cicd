a2a555a641885933d363536bf6424cddd497ef1e
vika5mj <37324318+vika5mj@users.noreply.github.com>
2024-10-09 13:29:03 +0530

    first commit for tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("credentials.json")
+  region = "us-central1"
+}
+
+resource "google_storage_bucket" "test_bucket" {
+  name          = "my-test-bucket"
+  location     = "us-central1"
+  uniform_bucket_level_access = true
+}
