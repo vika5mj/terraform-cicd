1af09cc243c82e0439e26bcd1b120426a99eb724
vika5mj <vikasmohan481@gmail.com>
2024-11-05 16:29:50 +0530

    test

---
+++ Dev/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "gcs" {
+    bucket = "terraform-iac-devops9700"
+    prefix = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "google" {
+  region = "us-central1"
+}
+
+# resource "google_storage_bucket" "test_bucket" {
+#   project   = "devops9700"
+#   name          = "my-test-bucketdevops9700-1"
+#   location     = "us-central1"
+#   uniform_bucket_level_access = true
+# }
