47677ec56e7f79a2611ce9bd741ce69178d85461
vika5mj <37324318+vika5mj@users.noreply.github.com>
2024-12-09 20:12:59 +0530

    Update bucket.tf

--- Dev/bucket.tf
+++ Dev/bucket.tf
@@ -1,5 +1,5 @@
 module "gcs_buckets" {
-  source  = "terraform-google-modules/cloud-storage/google"
+  source  = "git::https://github.com/vika5mj/gcp-bucket-module.git//bucket?ref=main"
   version = "~> 8.0"
   project_id  = "devops9700"
   names = ["first97001243", "second9700125434"]
